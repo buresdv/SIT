d8e594ac98a7b1f38b8ca3e9e55878f63c6e1d16
David Bureš <buresdv@gmail.com>
2016-11-08 13:06:58 +0100

    Bubble

--- Sort/InsertSort/src/insertsort/BubbleSort.java
+++ Sort/InsertSort/src/insertsort/BubbleSort.java
@@ -4,7 +4,7 @@ import insertsort.gui;
 import java.util.Arrays;
 import java.util.Date;
 
-public class InsertSort {
+public class BubbleSort {
     public static void main(String a[]) {
 
         int max = 100;
@@ -19,7 +19,7 @@ public class InsertSort {
         System.out.println("Vygenerovana cisla: " + Arrays.toString(array1));
 
 
-        int[] array2 = InsertSort(array1);
+        int[] array2 = BSort(array1);
         for(int i:array2) {
             System.out.print(i);
             System.out.print(", ");
@@ -29,23 +29,21 @@ public class InsertSort {
         Date konec = new Date();
         long rozdil = konec.getTime() - zacatek.getTime();
         System.out.println(rozdil + " milisekund");
-
         System.out.println("Pole o délce " + max + " náhodných čísel a maximálním číslu " + top + " bylo seřazeno za " + rozdil + " milisekund");
 
-  }
-    public static int[] InsertSort(int[] input) {
-        int temp;
-        //System.out.println("//DEBUG: Zacatek trideni");
-        for(int i = 1; i < input.length; i++) {
-            for(int j = i ; j > 0 ; j--) {
-                if(input[j] < input[j-1]) {
-                temp = input[j];
-                input[j] = input[j-1];
-                input[j-1] = temp;
-                    }
+    }
+
+    public static int[] BSort(int[] inputBubble) {
+        for (int i = 0; i < inputBubble.length - 1; i++) {
+            for (int j = 0; j < inputBubble.length - i - 1; j++) {
+                if(inputBubble[j] < inputBubble[j+1]) {
+                    int tmp = inputBubble[j];
+                    inputBubble[j] = inputBubble[j+1];
+                    inputBubble[j+1] = tmp;
                 }
             }
-        return input;
+        }
+        return inputBubble;
     }
 }
 
